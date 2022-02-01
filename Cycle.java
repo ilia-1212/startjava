91a278303ed5f6af737fc32a32db5bca1d833bf7
ILia-1212 <iliasentiakov@gmail.com>
2022-02-02 00:06:56 +0300

    fix err Cycle

--- Cycle.java
+++ Cycle.java
@@ -1,26 +1,26 @@
 public class Cycle{
-        public static void main(String [] args) {
-            System.out.println("Цикл for");
-            for(int x = 0; x <=20; x++) {
-                    System.out.println(x + "\n");
-            }
-
-            System.out.println("Цикл while");
-            int y = 6;
-            while (y >=-6) {
-                    System.out.println(y + "\n");
-                    y-=2;
-            }
-
-            System.out.println("Цикл do while");
-            int z = 10;
-            int summ = 0;
-            do {
-                    if (z % 2 != 0) {
-                        summ = summ + z;
-                    } 
-                    z++;
-            } while (z < 21);
-            System.out.println("сумма нечетных чисел '[10;20]' равна: " + summ);
+    public static void main(String[] args) {
+        System.out.println("Цикл for");
+        for(int i = 0; i <= 20; i++) {
+            System.out.println(i);
         }
+
+        System.out.println("Цикл while");
+        int y = 6;
+        while (y >=- 6) {
+            System.out.println(y);
+            y -= 2;
+        }
+
+        System.out.println("Цикл do while");
+        int z = 10;
+        int summOdd = 0;
+        do {
+            if (z % 2 != 0) {
+                summOdd =+ z;
+            } 
+            z++;
+        } while (z < 21);
+        System.out.println("сумма нечетных чисел '[10;20]' равна: " + summOdd);
+    }
 }
