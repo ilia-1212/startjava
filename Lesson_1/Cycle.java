cbae64314ca406fc3927000da9a1b20fb392dcbe
ILia-1212 <iliasentiakov@gmail.com>
2022-02-03 04:00:53 +0300

    fix err =+

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,4 +1,4 @@
-public class Cycle{
+public class Cycle {
     public static void main(String[] args) {
         System.out.println("Цикл for");
         for(int i = 0; i <= 20; i++) {
@@ -7,20 +7,21 @@ public class Cycle{
 
         System.out.println("Цикл while");
         int y = 6;
-        while (y >=- 6) {
+        while (y >= -6) {
             System.out.println(y);
             y -= 2;
         }
 
         System.out.println("Цикл do while");
         int z = 10;
-        int summOdd = 0;
+        int sumOdd = 0;
         do {
             if (z % 2 != 0) {
-                summOdd =+ z;
+                System.out.println("z= " + z);
+                sumOdd += z;
             } 
             z++;
         } while (z < 21);
-        System.out.println("сумма нечетных чисел '[10;20]' равна: " + summOdd);
+        System.out.println("сумма нечетных чисел '[10;20]' равна: " + sumOdd);
     }
 }
