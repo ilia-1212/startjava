3a5b081b72389b21c71ae32ac7a4e36a08a1bb33
ILia-1212 <iliasentiakov@gmail.com>
2022-03-21 18:10:12 +0300

    add Jaeger &JaegerTest

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,19 +1,81 @@
 public class Jaeger {
-    private String modelName = "Fedor";
-    private String mark = "Лютик-1";
-    private String origin = "венесуэла";
-    private float height = 70.7f;
-    private float weight = 2.1f;
-    private int strength = 8;
-    private int armor = 9;
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int strength;
+    private int armor;
+    private boolean isWeaponActivated;
 
+    //modelName
     public String getModelName() {
         return modelName;
     }
 
     public void setModelName(String modelName) {
-        if (modelName == "") {System.out.println("Пустое имя");}
+        if (modelName == "") {System.out.println("Пустое значение (имя)");}
         else this.modelName = modelName;
+    }
+
+    //mark
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        if (mark == "") {System.out.println("Пустое значение (модель)");}
+        else this.mark = mark;
+    }
+
+    //origin
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        if (origin == "") {System.out.println("Пустое значение (Страна производства)");}
+        else this.origin = origin;
+    }
+
+    //height
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        if (height <= 0f) {System.out.println("нулевое(или отрицательное) значение (Высота)");}
+        else this.height = height;
+    }
+
+    //weight
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if (weight <= 0f) {System.out.println("нулевое(или отрицательное) значение (Вес)");}
+        else this.weight = weight;
+    }
+
+    //strength
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        if (strength <= 0) {System.out.println("нулевое(или отрицательное) значение (прочность)");}
+        else this.strength = strength;
+    }
+
+    //armor
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        if (armor <= 0) {System.out.println("нулевое(или отрицательное) значение (вооружение)");}
+        else this.armor = armor;
     }
 
     boolean drift() {
@@ -29,7 +91,44 @@ public class Jaeger {
         return "scanning";
     }
 
-    void useVortexCannon() {
-        System.out.println("активирован");
+    void useCannon() {
+        if (modelName == "Bracer Phoenix") useVortexCannon();
+        else if (modelName == "Gipsy Danger") usePlasmaCannon();
+        else if (modelName == "Gipsy Danger") usePlasmaCannon();
+        else if (modelName == "Striker Eureka") useStingBladesCannon();
+        else System.out.println("вооружение отсутсвует");
     }
+
+    void useVortexCannon() {
+        if (isWeaponActivated) {
+            System.out.println("VortexCannon деактивирован");
+            isWeaponActivated = false;
+        }
+        else {
+            System.out.println("VortexCannon активирован");
+            isWeaponActivated = true;
+        }
+    }
+
+    void usePlasmaCannon() {
+        if (isWeaponActivated) {
+            System.out.println("PlasmaCannon деактивирован");
+            isWeaponActivated = false;
+        }
+        else {
+            System.out.println("PlasmaCannon активирован");
+            isWeaponActivated = true;
+        }
+    }
+
+    void useStingBladesCannon() {
+        if (isWeaponActivated) {
+            System.out.println("StingBladesCannon деактивирован");
+            isWeaponActivated = false;
+        }
+        else {
+            System.out.println("StingBladesCannon активирован");
+            isWeaponActivated = true;
+        }
+    }    
 }
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,8 +1,42 @@
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger rb1 = new Jaeger();
-        //String ff = rb1.getModelName();
-        rb1.setModelName("");
-        System.out.println("s "+ rb1.getModelName());
+        Jaeger robotOne = new Jaeger();
+        robotOne.setModelName("Bracer Phoenix");
+        robotOne.setMark("Mark-5");
+        robotOne.setOrigin("USA");
+        robotOne.setHeight(70.91f);
+        robotOne.setWeight(2.128f);
+        robotOne.setStrength(5);
+        robotOne.setArmor(4);
+        System.out.println(robotOne.getModelName());
+        System.out.println(robotOne.getArmor());
+        robotOne.useCannon();
+        robotOne.useCannon();
+        robotOne.setArmor(2);
+        System.out.println(robotOne.getArmor());
+        
+        Jaeger robotTwo = new Jaeger();
+        robotTwo.setModelName("Gipsy Danger");
+        robotTwo.setMark("Mark-3");
+        robotTwo.setOrigin("USA");
+        robotTwo.setHeight(79.25f);
+        robotTwo.setWeight(1.980f);
+        robotTwo.setStrength(8);
+        robotTwo.setArmor(6);
+        System.out.println(robotTwo.getModelName());
+        robotTwo.useCannon();
+        robotTwo.useCannon();
+
+        Jaeger robotThree= new Jaeger();
+        robotThree.setModelName("Striker Eureka");
+        robotThree.setMark("Mark-5");
+        robotThree.setOrigin("Australia");
+        robotThree.setHeight(76.2f);
+        robotThree.setWeight(1.850f);
+        robotThree.setStrength(10);
+        robotThree.setArmor(9);
+        System.out.println(robotThree.getModelName());
+        robotThree.useCannon();
+        robotThree.useCannon();
     }
 }
