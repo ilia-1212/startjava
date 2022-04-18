3213c2124382f286c89bc5bd6f9f4b6c762a1d4e
ILia-1212 <iliasentiakov@gmail.com>
2022-04-18 13:46:23 +0300

    fix errs Calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,21 +1,53 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
     private static String mathSign;
     private static int num1;
     private static int num2;
     private static float result;
+    private static String[] mathExpressions;
 
-    public static float calculate(String[] lines) {
-        num1 = Integer.parseInt(lines[0]);
-        mathSign = lines[1];
-        num2 = Integer.parseInt(lines[2]);
-
-        calculateExpression();
-        return result;
+    public static void addMathExpression(String mathExpression) {
+        Calculator.mathExpressions = mathExpression.split(" ");
     }
 
-    private static void calculateExpression() {
+    public static boolean checkExpression() {
+        return checkValidNumber(mathExpressions[0]) || checkValidSign(mathExpressions[1]) || checkValidNumber(mathExpressions[2]);
+    }
+
+    private static boolean checkValidNumber(String number) {
+        boolean isNonChecked = false;
+        if (number == null || number.isEmpty()) isNonChecked = true;
+        for(int i = 0; i < number.length(); i++) {
+            if (!Character.isDigit(number.charAt(i))) isNonChecked = true;
+        }
+
+        if (isNonChecked) {
+            System.out.println("Ошибка в числе [" + number + "]");
+        } else {
+            isNonChecked = !(Integer.parseInt(number) > 0 && Integer.parseInt(number) <= 100);
+        }
+        return isNonChecked;
+    }
+
+    private static boolean checkValidSign(String Sign) {
+        boolean isNonChecked = !(Sign.equals("+") ||
+                Sign.equals("-") ||
+                Sign.equals("*") ||
+                Sign.equals("/") ||
+                Sign.equals("^") ||
+                Sign.equals("%"));
+        if (isNonChecked) System.out.println("Ошибка в знаке операции [" + Sign + "]");
+        return isNonChecked;
+    }
+
+    public static float calculate() {
+
+        num1 = Integer.parseInt(mathExpressions[0]);
+        mathSign = mathExpressions[1];
+        num2 = Integer.parseInt(mathExpressions[2]);
         result = switch (mathSign) {
             case "+" ->  num1 + num2;
             case "-" ->  num1 - num2;
@@ -25,5 +57,6 @@ public class Calculator {
             case "%" ->  num1 % num2;
             default -> 0;
         };
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,59 +1,33 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CalculatorTest {
     private static Scanner scanner;
+    private static String playerAnswer = "";
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        String playerAnswer = "";
-
         while (!playerAnswer.equals("no")) {
-            System.out.print("Введите математическое выражение (используя пробелы): ");
-            System.out.printf("результат = " + "%.2f" + "\n", Calculator.calculate(inputExpression()));
+            System.out.println("Введите математическое выражение (используя пробелы): ");
+            inputExpression();
+            System.out.printf("результат = " + "%.2f" + "\n", Calculator.calculate());
             System.out.println("Хотите продолжить вычисления (yes/no)?");
-            while (!(playerAnswer = scanner.nextLine()).equals("yes") && !playerAnswer.equals("no")) {
+            while (checkAnswer()) {
                     System.out.println("Допустимы только значения: (yes/no)");
             }
         }
         scanner.close();
     }
 
-    private static String[] inputExpression() {
-        String[] lines;
+    private static void inputExpression() {
         do {
             System.out.println("число должно быть целое и положительное, диапазон (0, 100], а знак операции (+-*/^%)");
-            lines = scanner.nextLine().split(" ");
-        } while (isNonCheckedNum(lines[0]) || isNonCheckedOperation(lines[1]) || isNonCheckedNum(lines[2]));
-        return lines;
+            Calculator.addMathExpression(scanner.nextLine());
+        } while (Calculator.checkExpression());
     }
 
-    private static boolean isNonCheckedNum(String str) {
-        boolean isNonChecked = false;
-        if (str == null || str.isEmpty())   isNonChecked = true;
-        for(int i = 0; i < str.length(); i++) {
-            if  (!Character.isDigit(str.charAt(i)))  isNonChecked = true;
-        }
-
-        if (isNonChecked) {
-            System.out.println("Ошибка в числе [" + str + "]");
-        }
-        else {
-            isNonChecked = !(Integer.parseInt(str) > 0 && Integer.parseInt(str) <= 100);
-        }
-        return isNonChecked;
-    }
-
-    private static boolean isNonCheckedOperation(String str) {
-        boolean isNonChecked =  !(str.equals("+") ||
-                str.equals("-") ||
-                str.equals("*") ||
-                str.equals("/") ||
-                str.equals("^") ||
-                str.equals("%"));
-        if (isNonChecked) System.out.println("Ошибка в знаке операции [" + str + "]");
-        return isNonChecked;
+    private static boolean checkAnswer() {
+        return !(playerAnswer = scanner.nextLine()).equals("yes") && !playerAnswer.equals("no");
     }
 }
